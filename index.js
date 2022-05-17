438b405a32a77355488353d8c7bba6f3a8b2ddeb
Thomas Cook <tlncook85@gmail.com>
2022-05-16 21:32:51 -0600

    commit tree be8d6563861cbfef0b50852c36d4aef20266b8bd

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1 = 31;
+const num2 = 2;
+const num3 = 9;
+const num4 = 5;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+const mod = num3 % num4;
+const max = Math.max(5, 7, 12, 14, 20);
